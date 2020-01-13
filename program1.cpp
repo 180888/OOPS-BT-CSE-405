cbbe54cbca8aac26b6ef560760962b07346bb723
180888 <59637371+180888@users.noreply.github.com>
2020-01-13 13:10:05 +0530

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -3,24 +3,22 @@
 using namespace std;
 double power(double,int=2);
 void display(int);
-
 int main()
 {
-    int n,p,value;
-    cout<<"enter base number and exponents "<<endl;
-    cin>>n>>p;
-   value= power(n,p);
+  int m,n,value;
+   cout<<"enter base number and exponents "<<endl;
+   cin>>m>>n;
+   value= power(m,n);
    display(value);
    cout<<"\nwhen base value is 4 and no argument is passed for exponent\n";
    value=power(4);
    display(value);
     return 0;
-
 }
-double power(double n,int p)
+double power(double m,int n)
 { int power=1;
-for(int i=1;i<=p;i++)
-    power*=n;
+for(int i=1;i<=n;i++)
+    power*=m;
 return power;
 }
 void display(int c)
